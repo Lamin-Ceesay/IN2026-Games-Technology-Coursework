b85d877bb12a588d5704f679dfaf32635d6d3750
Lamin-Ceesay <lamin.ceesay.2@city.ac.uk>
2026-04-18 02:46:54 +0100

    asteroid collision
    asteroid collision added

--- IN2026 Coursework Code/SRC/Asteroid.cpp
+++ IN2026 Coursework Code/SRC/Asteroid.cpp
@@ -21,7 +21,7 @@ Asteroid::~Asteroid(void)
 
 bool Asteroid::CollisionTest(shared_ptr<GameObject> o)
 {
-	if (GetType() == o->GetType()) return false;
+	//if (GetType() == o->GetType()) return false;
 	if (mBoundingShape.get() == NULL) return false;
 	if (o->GetBoundingShape().get() == NULL) return false;
 	return mBoundingShape->CollisionTest(o->GetBoundingShape());
@@ -29,5 +29,46 @@ bool Asteroid::CollisionTest(shared_ptr<GameObject> o)
 
 void Asteroid::OnCollision(const GameObjectList& objects)
 {
-	mWorld->FlagForRemoval(GetThisPtr());
+	for (auto i = objects.begin();  i != objects.end(); ++i)
+	{
+		shared_ptr<GameObject> other = *i;
+		
+		if (other->GetType() == GameObjectType("Asteroid"))
+		{
+			GLVector3f position1 = GetPosition();
+			GLVector3f position2 = other->GetPosition();
+			GLVector3f velocity1 = GetVelocity();
+			GLVector3f velocity2 = other->GetVelocity();
+			
+			float posix = position1.x - position2.x;
+			float posiy = position1.y - position2.y;
+			float dist = sqrt((posix * posix) + (posiy * posiy));
+			if (dist == 0.0f) dist = 0.01f;
+			
+			float nx = posix/ dist;
+			float ny = posiy / dist;
+			float vn = (((velocity1.x - velocity2.x) * nx) + ((velocity1.y - velocity2.y) * ny));
+			
+			if (vn <0.0f)
+			{
+				velocity1.x -= vn * nx;
+				velocity1.y -= vn * ny;
+				velocity2.x += vn * nx;
+				velocity2.y += vn * ny;
+				
+				
+				SetVelocity(velocity1);
+				other->SetVelocity(velocity2);
+			} 
+			
+		}
+		else{
+			mWorld->FlagForRemoval(GetThisPtr());
+			
+		}
+	}
 }
+	
+
+	
+
